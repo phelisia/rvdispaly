22be9f66175259a5d5117448853760f63e595047
phelisa <phelisajeruto@gmail.com>
2022-07-17 22:53:06 +0300

    displayed image

--- app/src/main/java/dev/phelisia/mycontacts/ContactAdapter.kt
+++ app/src/main/java/dev/phelisia/mycontacts/ContactAdapter.kt
@@ -1,10 +1,13 @@
 package dev.phelisia.mycontacts
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
@@ -24,9 +27,28 @@ class ContactsAdapter(var contactList: List<Contact>):RecyclerView.Adapter<Conta
         holder.binding.tvemail.text=currentContact.email
         holder.binding.tvAddress.text=currentContact.address
 
-        Picasso.get().load(currentContact.image).placeholder(R.drawable.ic_baseline_person_24).error(R.drawable.ic_baseline_error_outline_24).resize(350,350).centerCrop().into(holder.binding.ivContact)
+        Picasso.get().load(currentContact.image)
+            .placeholder(R.drawable.ic_baseline_person_24)
+            .error(R.drawable.ic_baseline_error_outline_24)
+            .resize(350,350)
+            .centerCrop()
+            .into(holder.binding.ivContact)
+        val context=holder.itemView.context
+        holder.binding.ivContact.setOnClickListener {
+            Toast.makeText(context,"you have clicked on ${currentContact}the image",Toast.LENGTH_SHORT).show()
+        }
+holder.binding.cvContact.setOnClickListener{
+    val  intent=Intent(context,ViewContactActivity::class.java)
+    intent.putExtra("NAME",currentContact.name)
+    intent.putExtra("EMAIL",currentContact.email)
+    intent.putExtra("ADDRESS",currentContact.address)
+    intent.putExtra("PHONEE",currentContact.PhoneNumber)
+    intent.putExtra("IMAGE",currentContact.image)
+    context.startActivity(intent)
 
+}
     }
+
 
     override fun getItemCount(): Int {
         return  contactList.size
---
+++ app/src/main/java/dev/phelisia/mycontacts/ViewContactActivity.kt
@@ -0,0 +1,39 @@
+package dev.phelisia.mycontacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.squareup.picasso.Picasso
+import dev.phelisia.mycontacts.databinding.ActivityViewContactBinding
+
+class ViewContactActivity : AppCompatActivity() {
+    lateinit var binding: ActivityViewContactBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityViewContactBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getExtras()
+    }
+
+    private fun getExtras() {
+        val extras=intent.extras
+        val image = extras?.getString("IMAGE","")
+        Picasso.get().load(image)
+            .placeholder(R.drawable.ic_baseline_person_24)
+            .error(R.drawable.ic_baseline_error_outline_24)
+            .resize(350,350)
+            .centerCrop()
+            .into(binding.iv3)
+        val name=extras?.getString("NAME","")
+        binding.tvNamee.text = name
+        val  phone=extras?.getString("PHONEE","")
+        binding.tvphonee.text=phone
+        val email=extras?.getString("EMAIL","")
+        binding.tvemaill.text=email
+
+
+        Toast.makeText(this,"$name:$email",Toast.LENGTH_LONG).show()
+    }
+
+
+}
